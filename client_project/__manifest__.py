d0f5571546c5d1f50b18baf0eb38a740a7ad3e8d
EsdFatima <fessid43@gmail.com>
2024-03-02 22:24:57 +0100

    add client module

---
+++ client_project/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Products Color',
+    'version': '16.0.0.0',
+    'author': 'ESSID Fatima Zahraa',
+    'category': 'Sales',
+    'license': 'LGPL-3',
+    'summary': 'Management of Products',
+    'description': """
+    This module allows you to manag product and calculates the total price for each product.
+    """,
+    'depends': ['itex_product'],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'demo': [
+
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
